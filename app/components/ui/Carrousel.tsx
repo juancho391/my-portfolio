d83337dec03d9a718d319f89572da115e7724035
juancho391 <juanchischis2010@hotmail.com>
2025-05-27 21:43:38 -0500

    carousel para las imagenes de los proyectos

--- app/components/ui/Carrousel.tsx
+++ app/components/ui/Carrousel.tsx
@@ -3,6 +3,7 @@ import { IconArrowNarrowRight } from "@tabler/icons-react";
 import { useState, useRef, useId, useEffect } from "react";
 
 export interface SlideData {
+  id: number;
   src: string;
 }
 
@@ -13,61 +14,32 @@ interface SlideProps {
   handleSlideClick: (index: number) => void;
 }
 
-export function CurrentSlide() {
-  return (
-    <div className="absolute bottom-0 left-0 right-0 flex justify-center"></div>
-  );
-}
-const Slide = ({ slide, index, current, handleSlideClick }: SlideProps) => {
-  return <img className="w-30" src={slide.src} alt="imagen del carrousel" />;
-};
+export function Carousel({ slides }: { slides: SlideData[] }) {
+  const [current, setCurrent] = useState(1);
+  const currentSlide = slides.find((slide) => slide.id === current);
 
-interface CarouselControlProps {
-  type: string;
-  title: string;
-  handleClick: () => void;
-}
-
-// const CarouselControl = ({
-//   type,
-//   title,
-//   handleClick,
-// }: CarouselControlProps) => {
-//   return (
-//     <button
-//       className={`w-10 h-10 flex items-center mx-2 justify-center bg-neutral-200 dark:bg-neutral-800 border-3 border-transparent rounded-full focus:border-[#6D64F7] focus:outline-none hover:-translate-y-0.5 active:translate-y-0.5 transition duration-200 ${
-//         type === "previous" ? "" : ""
-//       }`}
-//       title={title}
-//       onClick={handleClick}
-//     >
-//       <IconArrowNarrowRight className="text-neutral-600 dark:text-neutral-200" />
-//     </button>
-//   );
-// };
-
-interface CarouselProps {
-  slides: SlideData[];
-}
-
-//Carrousel component that displays a list of slides
-export function Carousel({ slides }: CarouselProps) {
-  const [current, setCurrent] = useState(0);
-
-  const handleSlideClick = (index: number) => {
-    console.log(`Slide ${index} clicked`);
+  const handleClickSlide = (id: number) => {
+    console.log(id);
+    setCurrent(id);
   };
-
   return (
-    <div className="relative w-[70vmin] h-[70vmin] mx-auto">
-      <div className="flex flex-wrap justify-center w-full gap-1 ">
-        {slides.map((slide, index) => (
-          <Slide
-            key={index}
-            slide={slide}
-            index={index}
-            current={current}
-            handleSlideClick={handleSlideClick}
+    <div className="flex flex-col w-full h-full gap-3">
+      <div>
+        {currentSlide ? (
+          <img className="border-white border-1" src={currentSlide.src} />
+        ) : null}
+      </div>
+      <div className="w-full flex flex-wrap gap-2 items-center justify-center">
+        {slides.map((slide) => (
+          <img
+            src={slide.src}
+            key={slide.id}
+            className={`w-30 ${
+              slide.id === current
+                ? "border-3 border-white"
+                : "border-1 border-gray-300"
+            }`}
+            onClick={() => handleClickSlide(slide.id)}
           />
         ))}
       </div>
