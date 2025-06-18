11d94c87ba7f1ea9b077bb6ac69ad9d4dfbb21cf
juancho391 <juanchischis2010@hotmail.com>
2025-06-18 15:46:33 -0500

    quite el footer y lo puse en en layout, puse en el componente techCards para mostrar las tecnologias que uso

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,15 @@ import { Projects } from "@/app/components/Projects";
 import Contact from "./components/Contact";
 import projects from "@/app/projects/projects.json";
 import Testimonials from "./components/Testimonials";
-import { Footer } from "./components/Footer";
+import { TechCards } from "./components/TechStack";
 export default function Home() {
   return (
-    <div className="relative min-h-screen w-full bg-black overflow-x-hidden">
-      <div className="fixed inset-0 z-0">
-        <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]" />
-
-        <div className="absolute top-[-20%] left-[-20%] h-[1200px] w-[1200px] rounded-full bg-[radial-gradient(circle_400px_at_30%_300px,#fbfbfb36,transparent)]" />
-      </div>
-      <main className="relative z-10 flex flex-col items-center gap-16 p-8 sm:p-20">
-        <Hero />
-        {/* <Stack /> */}
-        <Projects projects={projects} />
-        <Contact />
-        <Testimonials />
-        <Footer />
-      </main>
-    </div>
+    <main className="relative z-10 flex flex-col items-center gap-16 p-8 sm:p-20">
+      <Hero />
+      <Projects projects={projects} />
+      <TechCards />
+      <Contact />
+      <Testimonials />
+    </main>
   );
 }
