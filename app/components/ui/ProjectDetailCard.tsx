212b350019834194cb8a4758a297bd15ea4fe345
juancho391 <juanchischis2010@hotmail.com>
2025-06-18 15:49:28 -0500

    componente para mostrar las teconologias que se usaron en un proyecot

--- app/components/ui/ProjectDetailCard.tsx
+++ app/components/ui/ProjectDetailCard.tsx
@@ -2,26 +2,62 @@
 import { TechContainer } from "./techContainer";
 import { Carousel } from "./Carrousel";
 import projects from "@/app/projects/projects.json";
-import { Tecnology } from "@/app/type";
+import { TechWithIcon } from "@/app/type";
+import { StackCard } from "./StackCard";
+// Icons
+import { SiTailwindcss } from "react-icons/si";
+import {
+  PythonIcon,
+  DockerLogo,
+  NextjsLogo,
+  FastAPiLogo,
+  PostgresqLogo,
+  DjangoIcon,
+  GitHubLogo,
+  GitLogo,
+  ReactLogo,
+} from "./StackCard";
+
 type Props = {
   link: string;
 };
 
 type PropsTechCard = {
-  tecnologies: Tecnology[];
-  name: string;
+  tecnologies: TechWithIcon[];
 };
-const TechCards = ({ tecnologies, name }: PropsTechCard) => {
-  return (
-    <div className="text-left flex flex-col gap-3">
-      <div className="w-20 bg-white h-1 rounded-full self-center"></div>
 
-      <h2 className="text-xl font-semibold">{name}:</h2>
-      <ol className="list-disc">
-        {tecnologies?.map((tool) => (
-          <li key={tool.id}>{tool.name}</li>
-        ))}
-      </ol>
+const iconsList = [
+  { id: 1, icon: <NextjsLogo /> },
+  { id: 2, icon: <PythonIcon /> },
+  { id: 3, icon: <DockerLogo /> },
+  { id: 4, icon: <FastAPiLogo /> },
+  { id: 5, icon: <PostgresqLogo /> },
+  { id: 6, icon: <GitLogo /> },
+  { id: 7, icon: <GitHubLogo /> },
+  { id: 8, icon: <SiTailwindcss className="text-5xl text-[#38B2AC]" /> },
+  { id: 9, icon: <DjangoIcon /> },
+  { id: 10, icon: <ReactLogo /> },
+];
+
+const TechCards = ({ tecnologies }: PropsTechCard) => {
+  const tecnologiesWithIcons = tecnologies.map((tech) => {
+    const icon = iconsList.find((icon) => icon.id === tech.id);
+    return { ...tech, icon: icon ? icon.icon : null };
+  });
+
+  return (
+    <div className="flex flex-wrap gap-3">
+      {tecnologiesWithIcons.map((tech) => (
+        <div
+          key={tech.id}
+          className="border-1 border-white rounded-lg p-2 h-25 text-center flex flex-col justify-center items-center gap-2 flex-grow-1"
+        >
+          <span className="flex items-center justify-center gap-2 grow-0">
+            {tech.icon}
+          </span>
+          <p>{tech.name}</p>
+        </div>
+      ))}
     </div>
   );
 };
@@ -29,10 +65,17 @@ const TechCards = ({ tecnologies, name }: PropsTechCard) => {
 export function ProjectDetailCard({ link }: Props) {
   const project = projects.find((project) => project.link === link);
 
-  if (!project) return <div>Proyecto no encontrado</div>;
+  if (!project) {
+    return <div>Proyecto no encontrado</div>;
+  }
+
+  const tecnologiesWithIcons = project.tecnologies.map((tech) => {
+    const icon = iconsList.find((icon) => icon.id === tech.id);
+    return { ...tech, icon: icon ? icon.icon : null };
+  });
 
   return (
-    <div className="flex flex-col items-center justify-center text-center w-full h-full gap-10 mt-5">
+    <div className="flex flex-col items-center justify-center text-center w-full h-full gap-10 mt-5 relative">
       <h1 className="font-extrabold text-2xl">{project.title}</h1>
       <div className="flex flex-wrap gap-3">
         {project.tecnologies.map((tech) => (
@@ -42,21 +85,17 @@ export function ProjectDetailCard({ link }: Props) {
       <div>
         <Carousel slides={project.images} />
       </div>
-      <div className="flex flex-col gap-5 w-full h-full ">
+      <div className="flex flex-col gap-5 w-full h-full items-center">
         <h1 className="text-2xl font-extrabold">Descripcion del proyecto</h1>
         <div className="w-20 bg-white h-1 rounded-full self-center"></div>
-
         <p>{project.descriptionDetail}</p>
         <h1 className="text-2xl font-extrabold">Desafios tecnicos</h1>
         <div className="w-20 bg-white h-1 rounded-full self-center"></div>
-
         <p>{project.desafio}</p>
         <h1 className="text-2xl font-extrabold">Tecnologias utilizadas</h1>
-        <TechCards tecnologies={project.tecnologiesBack} name={"Backend"} />
-        <TechCards tecnologies={project.tecnologiesFront} name={"Frontend"} />
-        <TechCards
-          tecnologies={project.additionalTools}
-          name={"Herramientas adicionales"}
+        <StackCard
+          description={tecnologiesWithIcons.map((tech) => tech.name).join(", ")}
+          tecnologies={tecnologiesWithIcons}
         />
       </div>
     </div>
