319f4db9e1a09866085085aa5b8bcb618bcbd744
juancho391 <juanchischis2010@hotmail.com>
2025-06-18 15:48:19 -0500

    informacion falsa para los testiminos (por ahora) luego pondre informacion real

--- app/components/Testimonials.tsx
+++ app/components/Testimonials.tsx
@@ -7,8 +7,8 @@ const Testimonials = () => {
     {
       quote:
         "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-      name: "Charles Dickens",
-      title: "A Tale of Two Cities",
+      name: "Jhostin Posada",
+      title: "GANADOR BECA UNILIBRE",
     },
     {
       quote:
