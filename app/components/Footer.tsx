e50221fd0fc62ff18e92f607ad855631afe7b216
juancho391 <juanchischis2010@hotmail.com>
2025-06-20 17:50:13 -0500

    footer responsive

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,15 +3,19 @@ import { FaGithub } from "react-icons/fa";
 import Link from "next/link";
 export const Footer = () => {
   return (
-    <footer className="flex items-center w-full justify-between p-10 relative">
-      <p>CopyRight © Juan Esteban Bedoya Vasquez</p>
-      <div className="flex gap-3">
-        <Link href={"https://www.linkedin.com/in/juan-esteban-bedoya-vasquez/"}>
-          <FaLinkedin className="text-4xl cursor-pointer" />
-        </Link>
-        <Link href={"https://github.com/juancho391"}>
-          <FaGithub className="text-4xl cursor-pointer" />
-        </Link>
+    <footer className="relative inline-block w-full">
+      <div className="flex items-center justify-between w-full p-10 gap-x-3">
+        <p>CopyRight © Juan Esteban Bedoya Vasquez</p>
+        <div className="gap-3 sm:flex">
+          <Link
+            href={"https://www.linkedin.com/in/juan-esteban-bedoya-vasquez/"}
+          >
+            <FaLinkedin className="text-4xl cursor-pointer" />
+          </Link>
+          <Link href={"https://github.com/juancho391"}>
+            <FaGithub className="text-4xl cursor-pointer" />
+          </Link>
+        </div>
       </div>
     </footer>
   );
